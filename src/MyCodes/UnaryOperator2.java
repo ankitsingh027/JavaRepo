9e8761388030a656b0a53c4f6de6f6a24146fc13
Ankit Singh <ankitsingh027@outlook.com>
2021-09-12 03:08:44 +0530

    initial commit

--- src/MyCodes/UnaryOperator2.java
+++ src/MyCodes/UnaryOperator2.java
@@ -1,7 +1,9 @@
+package MyCodes;
+
 public class UnaryOperator2 {
     public static void main(String[] args) {
         int x = 10;
         System.out.println(x++ + ++x);
-        System.out.println(x++ + ++x);
+        System.out.println(x++ + x++);
     }
 }
