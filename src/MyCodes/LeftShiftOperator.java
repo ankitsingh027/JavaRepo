715bea3256b97e553003155fa6d546103d30c132
Ankit Singh <ankitsingh027@outlook.com>
2021-09-18 23:09:02 +0530

    initial commit

--- src/MyCodes/LeftShiftOperator.java
+++ src/MyCodes/LeftShiftOperator.java
@@ -2,9 +2,9 @@ package MyCodes;
 
 public class LeftShiftOperator {
     public static void main(String[] args) {
-        System.out.println(10<<2);
-        System.out.println(10<<3);
-        System.out.println(10<<4);
-        System.out.println(10<<5);
+        System.out.println(10<<2); //10*2^2=40//
+        System.out.println(10<<3); //10*2^2=40//
+        System.out.println(10<<4); //10*2^2=40//
+        System.out.println(10<<5); //10*2^2=40//
     }
 }
