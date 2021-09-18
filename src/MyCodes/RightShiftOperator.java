8f2cba5b2effe4fa1fd7a0dcfc4939f019c8d8ed
Ankit Singh <ankitsingh027@outlook.com>
2021-09-18 23:22:46 +0530

    initial commit

---
+++ src/MyCodes/RightShiftOperator.java
@@ -0,0 +1,10 @@
+package MyCodes;
+
+public class RightShiftOperator {
+    public static void main(String[] args) {
+        System.out.println(10 >> 2); //10/2^2=40//
+        System.out.println(10 >> 3); //10/2^2=40//
+        System.out.println(10 >> 4); //10/2^2=40//
+        System.out.println(40 >> 5); //40/2^2=40//
+    }
+}
