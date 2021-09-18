52d06e3723ecddb1cb0cb7c4c9c05389dc83e9cf
Ankit Singh <ankitsingh027@outlook.com>
2021-09-18 23:01:04 +0530

    initial commit

---
+++ src/MyCodes/LeftShiftOperator.java
@@ -0,0 +1,10 @@
+package MyCodes;
+
+public class LeftShiftOperator {
+    public static void main(String[] args) {
+        System.out.println(10<<2);
+        System.out.println(10<<3);
+        System.out.println(10<<4);
+        System.out.println(10<<5);
+    }
+}
