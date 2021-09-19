259956e31f2cf325fde2ce432903e19d995a60da
Ankit Singh <ankitsingh027@outlook.com>
2021-09-19 07:07:30 +0530

    initial commit

---
+++ src/MyCodes/LogicalBitwiseORoperator.java
@@ -0,0 +1,16 @@
+package MyCodes;
+
+import java.util.Scanner;
+public class LogicalBitwiseORoperator {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number :");
+        int a = sc.nextInt();
+        System.out.println("Enter second number :");
+        int b =sc.nextInt();
+        System.out.println("Enter third number :");
+        int c =sc.nextInt();
+        System.out.println(a<b||a<c);
+        System.out.println(a<b|b<c);
+    }
+}
