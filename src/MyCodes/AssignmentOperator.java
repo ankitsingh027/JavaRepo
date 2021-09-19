79c0317a4735d54b9c301c571978c7bc2dd96c96
Ankit Singh <ankitsingh027@outlook.com>
2021-09-19 07:20:43 +0530

    initial commit

---
+++ src/MyCodes/AssignmentOperator.java
@@ -0,0 +1,12 @@
+package MyCodes;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a =10;
+        int b= 15;
+        a+=b;
+        b-=a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
