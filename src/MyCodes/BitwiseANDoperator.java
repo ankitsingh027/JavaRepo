6b62c0f525a5e6d0df9dd042f26c44b2dc31c883
Ankit Singh <ankitsingh027@outlook.com>
2021-09-19 06:43:53 +0530

    initial commit

---
+++ src/MyCodes/BitwiseANDoperator.java
@@ -0,0 +1,17 @@
+package MyCodes;
+
+import java.util.Scanner;
+import java.util.Scanner;
+public class BitwiseANDoperator {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number :");
+        int a = sc.nextInt();
+        System.out.println("Enter second number :");
+        int b =sc.nextInt();
+        System.out.println("Enter third number :");
+        int c =sc.nextInt();
+        System.out.println(a<b&a<c);
+        System.out.println(a<b&b<c);
+    }
+}
