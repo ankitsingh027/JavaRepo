25ca452408ae9d2584c6f12667d8620f531c0c76
Ankit Singh <ankitsingh027@outlook.com>
2021-09-20 22:44:48 +0530

    initial commit

---
+++ src/MyCodes/ifstattement.java
@@ -0,0 +1,10 @@
+package MyCodes;
+
+public class ifstattement {
+    public static void main(String[] args) {
+        int age=16;
+        if(age<18){
+            System.out.println("he can't get his driving license");
+        }
+    }
+}
