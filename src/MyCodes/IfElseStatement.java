8f641f96d05e05555b613271548c218dc97dcec4
Ankit Singh <ankitsingh027@outlook.com>
2021-09-20 22:51:09 +0530

    initial commit

---
+++ src/MyCodes/IfElseStatement.java
@@ -0,0 +1,13 @@
+package MyCodes;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age < 18){
+            System.out.println("He can get his driving license.\n");
+        }
+        else{
+            System.out.println("He can get his driving license.\n");
+        }
+    }
+}
