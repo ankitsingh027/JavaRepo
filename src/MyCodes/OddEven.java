183c3f61e57fdd386c96f552cab0a70fdd11a245
Ankit Singh <ankitsingh027@outlook.com>
2021-09-21 16:01:53 +0530

    initial commit

---
+++ src/MyCodes/OddEven.java
@@ -0,0 +1,18 @@
+package MyCodes;
+import java.util.Scanner;
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int a = sc.nextInt();
+        if (a%2==0)
+        {
+            System.out.println("the number is fucking  even\n");
+        }
+        else{
+            System.out.println("the number is fucking odd");
+        }
+
+
+    }
+}
