799f8e7907943049c74ae2ca0726a1aa425e96cc
Ankit Singh <ankitsingh027@outlook.com>
2021-09-22 00:37:32 +0530

    initial commit

---
+++ src/MyCodes/ifelseladder.java
@@ -0,0 +1,16 @@
+package MyCodes;
+
+public class ifelseladder {
+    public static void main(String[] args) {
+        int marks = 45;
+        if (marks>=90){
+            System.out.println("the student gets an A+");
+        }
+        else if (marks>=60&&marks<90){
+            System.out.println("the student gets a B+");
+        }
+else {
+    System.out.println("The student failed");
+        }
+    }
+}
