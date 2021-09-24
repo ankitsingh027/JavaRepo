4f7375f080547d6a898ec8c22db56926c426e418
Ankit Singh <ankitsingh027@outlook.com>
2021-09-24 22:09:16 +0530

    initial commit

---
+++ src/MyCodes/switchwithoutifelse.java
@@ -0,0 +1,20 @@
+package MyCodes;
+
+public class switchwithoutifelse {
+    public static void main(String[] args) {
+        //Java Switch Example where we are omitting the
+//break statement
+
+                int number=20;
+                //switch expression with int value
+                switch(number){
+                    //switch cases without break statements
+                    case 10: System.out.println("10");
+                    case 20: System.out.println("20");
+                    case 30: System.out.println("30");
+                    default:System.out.println("Not in 10, 20 or 30");
+                }
+            }
+        }
+
+
