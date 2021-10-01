7dd25bf8a2a8ff19698d25db8748ef20576a4c4b
Ankit Singh <ankitsingh027@outlook.com>
2021-10-01 08:23:31 +0530

    initial commit

---
+++ src/MyCodes/ForloopExample.java
@@ -0,0 +1,10 @@
+package MyCodes;
+
+public class ForloopExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            
+        }
+    }
+}
